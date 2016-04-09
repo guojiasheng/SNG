7fe6bc60824354190c82845663718326ccbf6b06
wangjs <wangjs_123456@126.com>
2016-04-09 15:25:38 +0800

    add

--- SNP/src/SNP.java
+++ SNP/src/SNP.java
@@ -3,9 +3,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Random;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Instances;
 
 public class SNP {
 
@@ -25,7 +33,7 @@ public class SNP {
 		
 		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(outArffFile), false), "utf-8"));
 		bufferedWriter.write("@relation sng.arff"+"\n");
-		for (i=0;i<=n+2;i++){
+		for (i=0;i<=2*n;i++){
 			bufferedWriter.write("@attribute feature"+i+"{I,L,V,F,M,C,A,G,P,T,S,Y,W,Q,N,H,E,D,K,R,B}" + "\n");
 		}
 		bufferedWriter.write("@attribute aa2"+i+"{I,L,V,F,M,C,A,G,P,T,S,Y,W,Q,N,H,E,D,K,R,B}" + "\n");
@@ -33,8 +41,10 @@ public class SNP {
 		bufferedWriter.write("@data" + "\n");
 
 		int beginPos,endPos;
+		int index=0;
 		while(InputLine != null)
 		{
+			index++;
 			String[] lineList = InputLine.split(",");
 			length=lineList.length;
 		    pos = Integer.parseInt(lineList[length-5]);
@@ -57,6 +67,14 @@ public class SNP {
 		    beginPos = pos-n-1;
 		    endPos = pos+n;
 		    extractSequence = sequence.substring(beginPos,endPos);
+		    
+		    boolean retval = extractSequence.contains("U");
+		    if(retval){
+		    	 InputLine = InputBR.readLine();
+		    	continue;
+		    }
+		    
+		    
 		    lable = lineList[length-2];
 		    aa2 = lineList[length-3];
 		    
@@ -70,15 +88,68 @@ public class SNP {
 		InputBR.close();
 	}
 	
+	public static Classifier getClassifier(String name){
+		Classifier classify = null;
+		switch (name){
+			case "rf":
+				 classify=new RandomForest();
+				 break;
+			case "j48":
+				 classify=new J48();
+				 break;
+			
+			default:
+				System.out.println("目前只支持rf(randomForst),j488");
+		}
+		return classify;
+	}
+	
+	public static double featureRate(String file) throws Exception{
+		FileReader reader=new FileReader(file);
+		Instances  data=new Instances(reader);
+		data.setClassIndex(data.numAttributes()-1);//设置训练集中，target的index  
+		Classifier classify=getClassifier("j48");
+		classify.buildClassifier(data);
+		Evaluation eval=new Evaluation(data);
+		eval.crossValidateModel(classify, data,10, new Random());
+		eval.evaluateModel(classify, data);
+		System.out.println(1-eval.errorRate());
+		return 1-eval.errorRate();
+		//System.out.println(String.valueOf(1-eval.errorRate()));
+		//System.out.println(eval.toSummaryString());
+		//System.out.println(eval.toMatrixString());
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//String command="-i D://工作//nsSNP_rawData//humvar_dataset.csv -o D://工作//nsSNP_rawData//out.arff -n 3";
 		//String com[] = command.split(" ");
 		
+		/*
+		double bestRate=0;
+		double temp;
+		int bestN=0;
+		for(int i =1; i<100 ; i++){
+			
+			String command="-i D://工作//nsSNP_rawData//humvar_dataset.csv -o D://工作//nsSNP_rawData//sng -n 3";
+			String com[] = command.split(" ");
+			n = Integer.valueOf(i);
+			String inputFile = com[1];
+			String outArff =com[3]+"_"+String.valueOf(n)+".arff";
+			readCsv(inputFile,outArff);
+			temp = featureRate(outArff);
+			if (temp>bestRate){
+				bestRate = temp;
+				bestN = i;
+			}
+			
+		}
+		System.out.println(String.valueOf(bestN)+" "+bestRate);*/
+
 		String inputFile = args[1];
 		String outArff = args[3];
 		n = Integer.valueOf(args[5]);
 		readCsv(inputFile,outArff);
 	}
-
 }
